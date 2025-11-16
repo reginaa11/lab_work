42f6739acb1d18428b130da66e46290dd3c8e7f8
Regina <rsimonenko012@gmail.com>
2025-11-16 16:14:30 +0700

    feat: add LSP package with Bird, Sparrow, and Penguin

---
+++ src/main/java/com/example/lab8/lsp/Bird.java
@@ -0,0 +1,9 @@
+package com.example.lab8.lsp;
+
+public abstract class Bird {
+    public abstract void eat();
+
+    public void fly() {
+        throw new UnsupportedOperationException("Эта птица не умеет летать");
+    }
+}
